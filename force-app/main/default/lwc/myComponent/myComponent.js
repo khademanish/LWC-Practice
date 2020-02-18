278da147a324156f84d58a90639a842b77fa5e7c
Manish Khade <manish_khade@persistent.co.in>
2020-02-18 15:31:25 +0530

    Initial commit

---
+++ force-app/main/default/lwc/myComponent/myComponent.js
@@ -0,0 +1,12 @@
+import { LightningElement, track } from 'lwc';
+
+/**
+ * Show an item
+ */
+
+export default class Child extends LightningElement {
+@track guestName = 'Guest';
+ handleChange(event){
+     this.guestName = event.target.value;
+ }
+}
